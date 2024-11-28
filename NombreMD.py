16272a6a986abaf8c8c56178bb81c547e3d3e430
Marcosdaniel9117 <marcosjanco91@gmail.com>
2024-11-27 20:28:36 -0400

    B3-P3

--- NombreMD.py
+++ NombreMD.py
@@ -1,8 +1,12 @@
-class Persona:
-    def __init__(self, n, a):
-        self.MarcosDaniel= n
-        self.Janco = a
+class Datos:
+    def __init__(self, Nombre, b, c,d):
+        self.Nombre= Nombre
+        self.Apellido = b
+        self.Edad = c
+        self.Profesion = d
     
     def mostrar(self):
-        print(f"Nombres: {self.MarcosDaniel}")
-        print(f"Apellidos: {self.Janco}")
+        print(f"Nombres: {self.Nombre}")
+        print(f"Apellidos: {self.Apellido}")
+        print(f"Edad: {self.Edad}")
+        print(f"Profesion: {self.Profesion}")
