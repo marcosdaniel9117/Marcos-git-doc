8b372276347495b15e8ae49c2398fb41325baa2f
Marcosdaniel9117 <marcosjanco91@gmail.com>
2024-11-27 20:37:24 -0400

    subiendo archivos

---
+++ practica3/## Muestra-de la practica 3.java
@@ -0,0 +1,7 @@
+## Muestra-de la practica 3
+public class Main {
+    public static void main(String[] args) {
+        Coche Coche-+'' = new Coche("Vitara", "Suzuki", "2013");
+        Coche.Mostrar();  // This will display the car's details.
+    }
+}
